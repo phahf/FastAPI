a5cc362b8642505d986f516913910bb2f541d994
phahf <philipp.hake@hochfrequenz.de>
2026-04-17 14:56:36 +0200

    Dependency injection and losing the minimal examples

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Path, Query
+from fastapi import FastAPI, HTTPException, Path, Query, Depends
 from pydantic import BaseModel
 from typing import Literal
 import sqlite3
@@ -14,37 +14,37 @@ from exceptions import (
 app = FastAPI()
 
 
-### Minimalbeispiele
+# ### Minimalbeispiele
 
-# Einfache Get-Anfragen 
-class StatusResponse(BaseModel):
-    status: str
+# # Einfache Get-Anfragen 
+# class StatusResponse(BaseModel):
+#     status: str
 
-@app.get("/")
-def read_root():
-    return {"status": "okkk"}
+# @app.get("/")
+# def read_root():
+#     return {"status": "okkk"}
 
-@app.get("/health", response_model=StatusResponse)
-def health():
-    return {"statuss": "healthy", "status": "yes"}
-
-
-# Get-Anfrage mit Parameter
-class GreetingResponse(BaseModel):
-    greeting: str
-
-@app.get("/greet", response_model=GreetingResponse)
-def greet(name: str):
-    return {"greeting": f"Hallo {name}"}
+# @app.get("/health", response_model=StatusResponse)
+# def health():
+#     return {"statuss": "healthy", "status": "yes"}
 
 
-# Post-Anfrage
-class EchoRequest(BaseModel):
-    message: str
+# # Get-Anfrage mit Parameter
+# class GreetingResponse(BaseModel):
+#     greeting: str
 
-@app.post("/echo")
-def echo(data: EchoRequest):
-    return data
+# @app.get("/greet", response_model=GreetingResponse)
+# def greet(name: str):
+#     return {"greeting": f"Hallo {name}"}
+
+
+# # Post-Anfrage
+# class EchoRequest(BaseModel):
+#     message: str
+
+# @app.post("/echo")
+# def echo(data: EchoRequest):
+#     return data
 
 
 ### Nutzerdatenbank mit Passwortüberprüfung
@@ -62,6 +62,11 @@ conn.cursor().execute(
 )
 conn.commit()
 
+# Dependency 
+def get_user_service() -> UserService:
+    return UserService(conn)
+
+
 # Benutzer hinzufügen
 class UserCreateRequest(BaseModel):
     username: str
@@ -72,8 +77,8 @@ class UserCreateResponse(BaseModel):
     username: str
 
 @app.post("/users", response_model=UserCreateResponse, status_code=201)
-def create_user(data: UserCreateRequest):
-    service = UserService(conn)
+def create_user(data: UserCreateRequest,
+                service:UserService = Depends(get_user_service) ):
     try:
         user = service.create_user(username = data.username,
                             password = data.password )
@@ -90,9 +95,8 @@ def create_user(data: UserCreateRequest):
 
 # User löschen
 @app.delete("/users/{user_id}", status_code=204)
-def delete_user(
-        user_id: int = Path(..., gt=0) ):
-    service = UserService(conn)
+def delete_user(user_id: int = Path(..., gt=0),
+                service:UserService = Depends(get_user_service) ):
     try:
         service.delete_user(user_id)
     except UserNotFound:
@@ -106,17 +110,15 @@ class UserReadResponse(BaseModel):
     username: str
 
 @app.get("/users", response_model=list[UserReadResponse])
-def list_users(
-    sort: Literal["id", "username"] = Query(
-            "id",
-            description="Sort order of users, default 'id'") ):
-    service = UserService(conn)
+def list_users(sort: Literal["id", "username"] =
+               Query("id",description="Sort order of users, default 'id'"),
+               service:UserService = Depends(get_user_service)  ):
     return service.get_users(sort)
 
 # Einzelnen User zurückgeben
 @app.get("/users/{user_id}", response_model=UserReadResponse)
-def get_user( user_id: int = Path(..., gt=0) ):
-    service = UserService(conn)
+def get_user(user_id: int = Path(..., gt=0),
+             service:UserService = Depends(get_user_service) ):
     try:
         return service.get_user_by_id(user_id)
     except UserNotFound:
@@ -131,8 +133,8 @@ class PasswordChangeRequest(BaseModel):
 @app.patch("/users/{user_id}/password", status_code=204)
 def change_password(
         user_id: int = Path(..., gt=0),
-        data: PasswordChangeRequest = ... ):
-    service = UserService(conn)
+        data: PasswordChangeRequest = ...,
+        service:UserService = Depends(get_user_service) ):
     try:
         service.change_password(user_id, 
                                 current_password = data.current_password,
